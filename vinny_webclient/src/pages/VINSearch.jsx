8693c7d3560f79299ab72d54c912d0b1a61ad148
Thomas X Kirrane <txkirrane@txkirrane.dev>
2022-06-01 21:55:25 -0600

    aesthetic changes

--- vinny_webclient/src/pages/VINSearch.jsx
+++ vinny_webclient/src/pages/VINSearch.jsx
@@ -8,8 +8,8 @@ export default function VINSearch(props){
     return(
         <div className="container p-4 h-100">
             <div className="row h-100 row-cols-1 justify-content-center align-items-center">
-                <div className="col d-flex flex-column gap-3">
-                    <h1 className="mb-4 text-center">Enter a VIN #</h1>
+                <div className="col d-flex flex-column gap-2">
+                    <h1 className="mb-4 text-center">VIN Lookup</h1>
                     <SearchForm placeholder="Enter a VIN #" handleSearch={(term) => navigate(`/vehicle/${term}`)} />
                     <p className="text-center text-muted">Built by Thomas Kirrane</p>
                 </div>
